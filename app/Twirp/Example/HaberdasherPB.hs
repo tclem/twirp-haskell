8727ad16ed0a2d2b2daab5c607593073d6a82ada
Timothy Clem <timothy.clem@gmail.com>
2019-02-08 09:46:41 -0800

    Commit generated file

--- app/Twirp/Example/HaberdasherPB.hs
+++ app/Twirp/Example/HaberdasherPB.hs
@@ -3,6 +3,8 @@
 module Twirp.Example.HaberdasherPB where
 
 import Data.Aeson
+import Data.ByteString (ByteString)
+import Data.Fixed (Fixed)
 import Data.Int
 import Data.Text (Text)
 import Data.Word
@@ -11,20 +13,27 @@ import Proto3.Suite
 
 data Size = Size
   { inches :: Int32
-  } deriving stock (Eq, Show, Generic)
+  } deriving stock (Eq, Ord, Show, Generic)
     deriving anyclass (Message, Named, FromJSON, ToJSON)
 
 data Hat = Hat
   { inches :: Int32
-  , color :: String
-  , name :: String
-  } deriving stock (Eq, Show, Generic)
+  , color :: Text
+  , name :: Text
+  } deriving stock (Eq, Ord, Show, Generic)
     deriving anyclass (Message, Named, FromJSON, ToJSON)
+
+data BillExtra
+  = VatInfo { vatInfo :: Text }
+  | ZipCode { zipCode :: Text }
+  deriving stock (Eq, Ord, Show, Generic)
+  deriving anyclass (Message, Named, FromJSON, ToJSON)
 
 data Bill = Bill
   { price :: Maybe Price
   , status :: BillingStatus
-  } deriving stock (Eq, Show, Generic)
+  , extra :: Maybe BillExtra
+  } deriving stock (Eq, Ord, Show, Generic)
     deriving anyclass (Message, Named, FromJSON, ToJSON)
 
 data BillingStatus
@@ -33,10 +42,10 @@ data BillingStatus
   deriving stock (Eq, Ord, Show, Enum, Bounded, Generic)
   deriving anyclass (Named, MessageField, FromJSON, ToJSON)
   deriving Primitive via PrimitiveEnum BillingStatus
-instance HasDefault BillingStatus where def = Paid
+instance HasDefault BillingStatus where def = UnPaid
 
 data Price = Price
   { dollars :: Word32
   , cents :: Word32
-  } deriving stock (Eq, Show, Generic)
+  } deriving stock (Eq, Ord, Show, Generic)
     deriving anyclass (Message, Named, FromJSON, ToJSON)
