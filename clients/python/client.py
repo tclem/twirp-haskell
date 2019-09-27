daa4a4b307f06be576b7295b9a087513e7ff4cc1
Timothy Clem <timothy.clem@gmail.com>
2019-09-27 08:42:37 -0700

    Fix for python3

--- clients/python/client.py
+++ clients/python/client.py
@@ -4,7 +4,7 @@ from haberdasher import *
 
 def run():
     client = HealthClient("http://localhost:8003")
-    print client.check(Ping(service="test"))
+    print(client.check(Ping(service="test")))
 
 if __name__ == '__main__':
     run()
