c93f37eef72500d9fc06c8f47567c016f2943531
Neil Mayhew <neil@fpcomplete.com>
2020-09-25 17:36:36 -0600

    Avoid overwriting Twirp error responses returned by the application

--- src/Twirp/Middleware/Errors.hs
+++ src/Twirp/Middleware/Errors.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE DerivingVia, DeriveAnyClass, ScopedTypeVariables #-}
+{-# LANGUAGE DerivingVia, DeriveAnyClass, ScopedTypeVariables, ViewPatterns #-}
 module Twirp.Middleware.Errors where
 
 import Data.Aeson
@@ -6,28 +6,41 @@ import GHC.Generics
 import Network.HTTP.Types
 import Network.Wai
 
+import qualified Data.ByteString as BS
+
 data TwirpError = TwirpError { code :: String, msg :: String }
   deriving stock (Eq, Show, Generic)
   deriving anyclass (ToJSON)
 
--- Rewrite error responses to use Twirp's error codes and JSON encoding.
+-- | Rewrite error responses to use Twirp's error codes and JSON encoding
+-- when they don't already fit that model.
 -- See: https://github.com/twitchtv/twirp/blob/master/docs/errors.md
 twirpErrorResponses :: Middleware
-twirpErrorResponses = modifyResponse go
+twirpErrorResponses = modifyResponse $ \response ->
+  if nonTwirpError response then
+    let
+      status = responseStatus response
+      errResponse err = responseLBS status headers (encode err)
+    in case statusCode status of
+      400 -> errResponse badRequest
+      404 -> errResponse notFound
+      408 -> errResponse canceled
+      500 -> errResponse serverError
+      503 -> errResponse unavailable
+      _   -> errResponse unknown
+  else
+    response
   where
-    go response = let status = responseStatus response in
-      case statusCode status of
-        400 -> responseLBS status headers (encode badRequest)
-        404 -> responseLBS status headers (encode notFound)
-        408 -> responseLBS status headers (encode canceled)
-        500 -> responseLBS status headers (encode serverError)
-        503 -> responseLBS status headers (encode unavailable)
-        _   -> response
+    nonTwirpError r = isError r && not (isJson r)
+    isError (responseStatus -> s) = statusIsClientError s || statusIsServerError s
+    isJson (responseHeaders -> hs) = maybe False ("application/json" `BS.isPrefixOf`) $
+      lookup hContentType hs
 
     headers = [(hContentType, "application/json; charset=utf-8")]
 
     badRequest = TwirpError "invalid_argument" "Bad Request"
     notFound = TwirpError "not_found" "Not found"
     canceled = TwirpError "canceled" "Request Timeout"
-    unavailable = TwirpError "unavailable" "Service Unavailable"
     serverError = TwirpError "internal" "Internal Server Error"
+    unavailable = TwirpError "unavailable" "Service Unavailable"
+    unknown = TwirpError "unknown" "Unknown"
